51f4f0d5e8f8c661e4739da9d79173077305a769
civ <civ@room79.c-iv.ru>
2026-08-09 11:00:05 +0700

    Add AMD ROCm/HIP support

---
+++ engine/src/gpu.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#if defined (GGML_USE_CUDA)
+  #if defined (__HIP_PLATFORM_AMD__)
+     #include <hip/hip_runtime.h>
+     #define cudaMemGetInfo hipMemGetInfo
+     #define cudaGetErrorString hipGetErrorString
+     #define cudaError_t hipError_t
+     #define cudaSuccess hipSuccess
+  #else defined (GGML_USE_CUDA)
+     #include <cuda_runtime.h>
+  #endif
+#endif
